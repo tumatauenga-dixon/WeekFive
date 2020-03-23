042e64cf7c66a779345285e82e1482091784146a
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-23 14:58:16 +1300

    Added

--- index.js
+++ index.js
@@ -72,3 +72,15 @@ console.log(value);
 let array = [0, 1, 2];
 for (let value of array)
    console.log(value);
+
+//The .key() takes out the keys of the objects to be used later. The value is attached to the keys so that you can use the key to find the value.
+   const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.keys(object1));
+  // expected output: Array ["a", "b", "c"]
+
+  
